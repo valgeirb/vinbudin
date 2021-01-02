d37c45c5c4837600733e387cce6bf06013e69abd
Valgeir Bjornsson <valgeirb@valka.is>
2021-01-02 15:01:30 +0000

    All beers can now be scraped to a data.json file

--- src/pageController.js
+++ src/pageController.js
@@ -1,9 +1,48 @@
+const fs = require("fs");
 const pageScraper = require("./pageScraper");
 async function scrapeAll(browserInstance) {
   let browser;
   try {
     browser = await browserInstance;
-    await pageScraper.scraper(browser, "Bjor");
+
+    const baseUrlBeers =
+      "https://www.vinbudin.is/english/home/products/vorur.aspx/?category=beer";
+
+    const allBeers = await pageScraper.scraper(browser, baseUrlBeers);
+
+    /* const christmasBeers = await pageScraper.scraper(
+      browser,
+      `${baseUrlBeers}&season=J%C3%93L`,
+    );
+
+    const octoberBeers = await pageScraper.scraper(
+      browser,
+      `${baseUrlBeers}&season=OKT%C3%93BER`,
+    );
+
+    const easterBeers = await pageScraper.scraper(
+      browser,
+      `${baseUrlBeers}&season=P%C3%81SKAR`,
+    );
+
+    const thorriBeers = await pageScraper.scraper(
+      browser,
+      `${baseUrlBeers}&season=%C3%9EORRI`,
+    ); */
+
+    await browser.close();
+
+    let scrapedData = {
+      allBeers,
+    };
+
+    fs.writeFile("data.json", JSON.stringify(scrapedData), "utf8", (err) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log("Scrape complete, see './data.json'");
+    });
   } catch (err) {
     console.log("Could not resolve the browser instance => ", err);
   }
--- src/pageScraper.js
+++ src/pageScraper.js
@@ -1,120 +1,132 @@
 const scraperObject = {
-  url: "https://www.vinbudin.is/heim/vorur",
-  async scraper(browser, category) {
+  async scraper(browser, startingUrl) {
     let page = await browser.newPage();
-    console.log(`Navigating to ${this.url}...`);
-    await page.goto(this.url);
-    await page.waitForSelector(".category-links-wrapper");
+    await page.goto(startingUrl);
+    let scrapedData = [];
 
-    let scrapedItems = [];
+    const scrapeCurrentPage = async () => {
+      console.log(`Navigating to ${await page.url()}`);
+      await page.waitForSelector(".inner");
+      let productLinks = await page.$$eval("ul > li.product", (links) => {
+        links = links.map((el) => el.querySelector("li > a").href);
 
-    let categories = await page.$$eval(
-      `.voru-link-wrapper.${category} > div`,
-      (links) => {
-        links = links.map((el) => el.querySelector("div > a").href);
         return links;
-      },
-    );
-
-    const getAllItems = (link) =>
-      new Promise(async (resolve, reject) => {
-        let newPage = await browser.newPage();
-        await newPage.goto(link);
-        const bla = await newPage.$$eval("ul > li.product", (links) => {
-          links = links.map((el) => el.querySelector("li > a").href);
-          return links;
-        });
-
-        resolve(bla);
-        await newPage.close();
       });
 
-    let getSingleItem = (link) =>
-      new Promise(async (resolve, reject) => {
-        let newPage = await browser.newPage();
-        await newPage.goto(link);
+      let getSingleItem = (url) =>
+        new Promise(async (resolve, reject) => {
+          let newPage = await browser.newPage();
+          await newPage.goto(url);
 
-        const title = await newPage.$eval(
-          "h3 > span.product-info-text",
-          (text) => text.textContent,
-        );
+          const title = await newPage.$eval(
+            "h3 > span.product-info-text",
+            (text) => text.textContent,
+          );
 
-        const productId = await newPage.$eval(
-          "h3 > span.product-info-text.product-number-text",
-          (text) => text.textContent.replace(/[()]/g, ""),
-        );
+          const productId = await newPage.$eval(
+            "h3 > span.product-info-text.product-number-text",
+            (text) => text.textContent.replace(/[()]/g, ""),
+          );
 
-        const price = await newPage.$eval(
-          "div.price > span.money",
-          (text) => text.textContent,
-        );
+          const price = await newPage.$eval(
+            "div.price > span.money",
+            (text) => text.textContent,
+          );
 
-        const pricePerLiter = await newPage.$eval(
-          "div.price > span.price-per-liter",
-          (text) => text.textContent.replace(/[^0-9.]/g, ""),
-        );
+          const pricePerLiter = await newPage.$eval(
+            "div.price > span.price-per-liter",
+            (text) => text.textContent.replace(/[^0-9.]/g, ""),
+          );
 
-        const alcoholByVolume = await newPage.$eval(
-          "span#ctl01_ctl01_Label_ProductAlchoholVolume",
-          (text) => text.textContent,
-        );
+          const alcoholByVolume = await newPage.$eval(
+            "span#ctl01_ctl01_Label_ProductAlchoholVolume",
+            (text) => text.textContent,
+          );
 
-        const countryOfOrigin = await newPage.$eval(
-          "span#ctl01_ctl01_Label_ProductCountryOfOrigin",
-          (text) => text.textContent,
-        );
+          const countryOfOrigin = await newPage.$eval(
+            "span#ctl01_ctl01_Label_ProductCountryOfOrigin",
+            (text) => text.textContent,
+          );
 
-        const producer = await newPage.$eval(
-          "span#ctl01_ctl01_Label_Producer",
-          (text) => text.textContent,
-        );
+          const producer = await newPage.$eval(
+            "span#ctl01_ctl01_Label_Producer",
+            (text) => text.textContent,
+          );
 
-        const wholeSaler = await newPage.$eval(
-          "span#ctl01_ctl01_Label_ProductSeller",
-          (text) => text.textContent,
-        );
+          const supplier = await newPage.$eval(
+            "span#ctl01_ctl01_Label_ProductSeller",
+            (text) => text.textContent,
+          );
 
-        const packaging = await newPage.$eval(
-          "span#ctl01_ctl01_Label_ProductPackaging",
-          (text) => text.textContent,
-        );
+          let packaging = "";
 
-        const volume = await newPage.$eval(
-          "span#ctl01_ctl01_Label_ProductBottledVolume",
-          (text) => text.textContent,
-        );
+          try {
+            packaging = await newPage.$eval(
+              "span#ctl01_ctl01_Label_ProductPackaging",
+              (text) => text.textContent,
+            );
+          } catch (error) {
+            packaging = "n/a";
+          }
 
-        const imageUrl = await newPage.$eval(
-          "div.slick-slide.slick-current.slick-active div div a",
-          (el) => el.href,
-        );
+          const volume = await newPage.$eval(
+            "span#ctl01_ctl01_Label_ProductBottledVolume",
+            (text) => text.textContent,
+          );
 
-        resolve({
-          title,
-          productId,
-          price,
-          pricePerLiter,
-          alcoholByVolume,
-          countryOfOrigin,
-          producer,
-          wholeSaler,
-          packaging,
-          volume,
-          imageUrl,
+          let imageUrl = "";
+
+          try {
+            const url = await newPage.$eval(
+              "div.slick-slide.slick-current.slick-active div div a",
+              (el) => el.href,
+            );
+
+            imageUrl = url;
+          } catch (error) {
+            imageUrl = "n/a";
+          }
+
+          resolve({
+            title,
+            productId,
+            price,
+            pricePerLiter,
+            alcoholByVolume,
+            countryOfOrigin,
+            producer,
+            supplier,
+            packaging,
+            volume,
+            imageUrl,
+          });
+
+          await newPage.close();
         });
 
-        await newPage.close();
-      });
+      for (link of productLinks) {
+        let currentPageData = await getSingleItem(link);
+        scrapedData.push(currentPageData);
+      }
 
-    for (link in categories) {
-      let items = await getAllItems(categories[link]);
-      scrapedItems.push(items);
-    }
+      const isDisabled =
+        (await page.$("ul.pagination li:last-child.disabled")) !== null;
 
-    for (const link of scrapedItems.flat()) {
-      let item = await getSingleItem(link);
-      console.log(item);
-    }
+      if (!isDisabled) {
+        await page.click("ul.pagination li:last-child");
+        await page.waitForSelector(".loader", { visible: true });
+        await page.waitForSelector(".loader", { visible: false });
+
+        return scrapeCurrentPage();
+      }
+
+      await page.close();
+
+      return scrapedData;
+    };
+
+    let data = await scrapeCurrentPage();
+    return data;
   },
 };
 
